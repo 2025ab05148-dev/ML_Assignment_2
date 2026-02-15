dc124f05ee361d8d17a96d1b9b7b53b7d7362a40
2025ab05148-dev <2025ab05148@wilp.bits-pilani.ac.in>
2026-02-15 10:09:49 +0530

    Fixed model folder path

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ st.title("Breast Cancer Prediction Web Application")
 
 uploaded_file = st.file_uploader("Upload CSV File", type=["csv"])
 
-model_choice = st.selectbox(
-    "Select Model",
+models_choice = st.selectbox(
+    "Select models",
     [
         "Logistic_Regression",
         "Decision_Tree",
@@ -30,16 +30,16 @@ if uploaded_file is not None:
 
     # Safe path handling for Streamlit Cloud
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    model_folder = os.path.join(BASE_DIR, "model")
+    models_folder = os.path.join(BASE_DIR, "models")
 
-    scaler_path = os.path.join(model_folder, "scaler.pkl")
-    model_path = os.path.join(model_folder, f"{model_choice}.pkl")
+    scaler_path = os.path.join(models_folder, "scaler.pkl")
+    models_path = os.path.join(models_folder, f"{models_choice}.pkl")
 
     scaler = joblib.load(scaler_path)
-    model = joblib.load(model_path)
+    models = joblib.load(models_path)
 
     X = scaler.transform(df)
-    predictions = model.predict(X)
+    predictions = models.predict(X)
 
     df["Prediction"] = predictions
 
